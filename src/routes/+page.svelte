56b70a03f7bc63907cc700c75959471a77308e9e
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-08-11 21:39:57 -0400

    oops i guess i should probably commit soon maybe idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,57 @@
 <script>
-    import "../app.css";
+	import '../app.css';
+	import Trash from '$lib/Trash.svelte';
+	import { flip } from 'svelte/animate';
+	import { fade } from 'svelte/transition';
 
-    let count = $state(0);
+	const todos = $state([]);
+
+	let uid = todos.length + 1;
+
+	function add(desc) {
+		todos.push({
+			done: false,
+			text: desc,
+			id: uid++
+		});
+	}
+
+	function remove(id) {
+		const toDeleteIndex = todos.findIndex((todo) => todo.id === id);
+		todos.splice(toDeleteIndex, 1);
+	}
 </script>
-<div class="flex justify-center items-center h-screen bg-slate-900">
-    <h1 class="m-4 bg-slate-800 px-16 py-4 rounded-sm shadow-lg text-2xl text-white">Hello World!</h1>
-    <button onclick={count++} class="m-4 bg-slate-800 px-16 py-4 rounded-sm shadow-lg text-2xl text-white">{count}</button>
-    
+
+<div class="min-h-screen bg-slate-900 flex flex-col">
+	<div class="m-auto flex h-64 flex-none w-1/2 items-center">
+		<input
+			type="text"
+			class="w-full rounded-2xl bg-slate-800 p-4 text-white shadow-2xl transition hover:scale-103"
+			placeholder="what to do?"
+			onkeydown={(e) => {
+				if (e.key !== 'Enter') return;
+				add(e.currentTarget.value);
+				e.currentTarget.value = '';
+			}}
+		/>
+	</div>
+	<div class="m-auto flex-1 w-1/2">
+		{#each todos.toSorted((a, b) => Number(a.done) - Number(b.done)) as todo (todo.id)}
+			<div class="my-4 flex rounded-2xl bg-slate-800 p-4 shadow-2xl" transition:fade animate:flip>
+				<input type="checkbox" bind:checked={todo.done} />
+				<div
+					class={[
+						'mx-4',
+						{ 'text-slate-400 line-through': todo.done },
+						{ 'text-white': !todo.done }
+					]}
+				>
+					{todo.text}
+				</div>
+				<button onclick={() => remove(todo.id)} class="flex-auto flex justify-end">
+					<Trash stroke="white"/>
+				</button>
+			</div>
+		{/each}
+	</div>
 </div>
