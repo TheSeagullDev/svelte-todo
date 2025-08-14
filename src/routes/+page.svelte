e2507c72310b627ac455dedd6c8fd0e009f325eb
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-08-13 21:46:39 -0400

    Add editing?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
 	import Trash from '$lib/Trash.svelte';
+	import Check from '$lib/Check.svelte';
+	import Edit from '$lib/Edit.svelte';
 	import { flip } from 'svelte/animate';
 	import { fade } from 'svelte/transition';
 
@@ -12,7 +14,9 @@
 		todos.push({
 			done: false,
 			text: desc,
-			id: uid++
+			editing: false,
+			id: uid++,
+			contentElement: null
 		});
 	}
 
@@ -22,8 +26,8 @@
 	}
 </script>
 
-<div class="min-h-screen bg-slate-900 flex flex-col">
-	<div class="m-auto flex h-64 flex-none w-1/2 items-center">
+<div class="flex min-h-screen flex-col bg-slate-900">
+	<div class="m-auto flex h-64 w-1/2 flex-none items-center">
 		<input
 			type="text"
 			class="w-full rounded-2xl bg-slate-800 p-4 text-white shadow-2xl transition hover:scale-103"
@@ -35,22 +39,58 @@
 			}}
 		/>
 	</div>
-	<div class="m-auto flex-1 w-1/2">
+	<div class="m-auto w-1/2 flex-1">
 		{#each todos.toSorted((a, b) => Number(a.done) - Number(b.done)) as todo (todo.id)}
 			<div class="my-4 flex rounded-2xl bg-slate-800 p-4 shadow-2xl" transition:fade animate:flip>
 				<input type="checkbox" bind:checked={todo.done} />
+				{#if !todo.editing}
 				<div
+					bind:this={todo.contentElement}
 					class={[
-						'mx-4',
+						'mx-4 flex-1 rounded-md p-1',
 						{ 'text-slate-400 line-through': todo.done },
-						{ 'text-white': !todo.done }
+						{ 'text-white': !todo.done },
+						{ 'bg-slate-600': todo.editing}
 					]}
 				>
 					{todo.text}
 				</div>
-				<button onclick={() => remove(todo.id)} class="flex-auto flex justify-end">
-					<Trash stroke="white"/>
-				</button>
+				{:else}
+				<input
+					bind:this={todo.contentElement}
+					bind:value={todo.text}
+					onkeydown={(e) => {
+						if(e.key === "Enter") {
+							todo.editing = false;
+						}
+					}}
+					class={[
+						'mx-4 flex-1 rounded-md p-1',
+						{ 'text-slate-400 line-through': todo.done },
+						{ 'text-white': !todo.done },
+						{ 'bg-slate-600': todo.editing}
+					]}
+				>
+				{/if}
+				<div class="flex flex-auto justify-end">
+					{#if !todo.editing}
+						<button
+							onclick={() => {
+								todo.editing = true;
+								setTimeout(() => todo.contentElement.focus(), 0);
+							}}
+						>
+							<Edit stroke="white" />
+						</button>
+					{:else}
+						<button onclick={() => (todo.editing = false)}>
+							<Check stroke="white" />
+						</button>
+					{/if}
+					<button onclick={() => remove(todo.id)}>
+						<Trash stroke="white" />
+					</button>
+				</div>
 			</div>
 		{/each}
 	</div>
